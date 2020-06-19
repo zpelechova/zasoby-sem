84445c677ab7db9d44bf034d1b736322c36df36d
Zuzana Pelechová <zpelechova@gmail.com>
2020-06-19 23:47:16 +0200

    connecting form for delivery companies

--- apicodes/mail.js
+++ apicodes/mail.js
@@ -17,10 +17,23 @@ new Vue({
         Password: "czechitas2020",
         To: this.email,
         From: this.myMail,
-        Subject: "Vítejte na Zásoby sem",
-        Body: `Vítejte na zásoby sem! Váš obchod ${this.shopName} už bude brzy rozvážet s námi, hurá! Co nejdříve se s Vámi spojíme! Tým Zásoby sem `,
+        Subject: "novy Vítejte na Zásoby sem",
+        Body: `<div
+        id="mailToCust"
+        style="font-size: larger; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: #9d9065; display:inline-block; background-color: #fff9e8; border-radius: 10px; padding: 30px;"
+      >
+        <h2>Vítejte na zásoby sem!</h2>
+        <p>
+          Váš obchod <strong>${this.shopName}</strong> už bude možná brzy rozvážet
+          s námi,
+          <strong>hurá!</strong>
+        </p>
+        <p>Co nejdříve se s Vámi spojíme!</p>
+        <p><strong>Tým Zásoby sem</strong></p>
+      </div>`
       })
       // sending email to myself about the new request
+      const FormattedMessage = this.message ? `P.S.: Abych nezapomněla, ještě vzkazují: "${this.message}".` : "";
       Email.send({
         Host: "smtp.gmail.com",
         Username: this.myMail,
@@ -28,17 +41,42 @@ new Vue({
         To: this.myMail,
         From: this.myMail,
         Subject: "Další obchod chce k nám na stránku!",
-        Body: `Hurááá. Otevírají se nám nové obzory! Právě jsme obdrželi novou žádost o zařazení obchodu ${this.shopName} mezi naše zásobovače! Koukni na jejich webové stránky tady: ${this.webPage}. A pak honem napiš na email ${this.email}, kontaktní osoba je ${this.name}. Zásoby sem jedou!!! P.S.: Abych nezapomněla, ještě vzkazují: "${this.message}".`,
+        Body: `<div
+        id="mailToMe"
+        style="font-size: larger; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: #9d9065; display:inline-block; background-color: #fff9e8; border-radius: 10px; padding: 30px;"
+              >
+        <h2>Hurááá. Otevírají se nám nové obzory!</h2>
+        <p>
+          Právě jsme obdrželi novou žádost o zařazení obchodu
+          <strong>${this.shopName}</strong> mezi naše zásobovače!
+        </p>
+        <p>
+          Koukni na jejich webové stránky
+          <span style="text-decoration: underline"
+            ><a v-bind:href="${this.webPage}" target="_blank">tady</a></span
+          >, a ještě tady: ${this.webPage}.
+        </p>
+        <p>
+          A pak honem napiš na
+          <span>
+            <a v-bind:href="mailto:${this.email}">tento email</a> anebo na tento: ${this.email}
+          </span>
+          , kontaktní osoba je
+          <strong> ${this.name}.</strong>
+        </p>
+        <p><strong>Zásoby sem jedou!!!</strong></p>
+        <p>${FormattedMessage}</p>
+      </div>`,
       }).then(
         message => alert("Už to k nám jede! Těšíme se, jak to spolu rozjedem!")
       )
 
     }
   },
-//   mounted() {
-//     let input = document.querySelector('[autofocus]');
-//     if (input) {
-//         input.focus()
-//     }
-// }
+  //   mounted() {
+  //     let input = document.querySelector('[autofocus]');
+  //     if (input) {
+  //         input.focus()
+  //     }
+  // }
 });
--- src/components/pro_rozvozce.vue
+++ src/components/pro_rozvozce.vue
@@ -2,21 +2,56 @@
   <div class="pro__rozvozce__container">
     <div class="background__image">
       <div class="content">
-        <p>KONTAKTUJTE NÁS</p>
-        <p>Také rozvážíte zboží na adresy a chtěli byste být součástí srovnávání rozvozů na našich stránkách? Napište nám.</p>
-        <form class="let__us__know">
-          <div class="name__eshop">
-          <input type="text" class="name" name="name" placeholder="JMÉNO" />
-          <input type="text" class="eshop__address" name="eshop__address" placeholder="ADRESA E-SHOPU" />
-          </div>
-          <div class="person__email"> 
-          <input type="text" class="person" name="person" placeholder="KONTAKTNÍ OSOBA"/>
-          <input type="text" class="email" name="email" placeholder="E-MAIL" />
-          </div>
-          <div class="phone__btn">
-          <input type="text" class="phone__number" name="phone__number" placeholder="TELEFON" />
-          <button class="btn" type="button">POSLAT</button>
-          </div>
+        <p>Chcete, aby naše skvělá stránka nabízela zákazníkům i Váš obchod?</p>
+        <p>Vyplňte formulář a my se Vám ozveme.</p>
+        <form class="let__us__know" method="post" v-on:submit.prevent="sendEmails">
+          <input
+            type="text"
+            class="form_item"
+            name="name"
+            id="name"
+            v-model="name"
+            placeholder="JMÉNO*"
+            required
+            autofocus
+          />
+          <input
+            type="email"
+            class="form_item"
+            name="email"
+            id="email"
+            v-model="email"
+            placeholder="E-MAIL*"
+            required
+          />
+          <input
+            type="text"
+            class="form_item"
+            name="shopName"
+            id="shopName"
+            v-model="shopName"
+            placeholder="NÁZEV OBCHODU*"
+            required
+          />
+          <input
+            type="url"
+            class="form_item"
+            name="webPage"
+            id="webPage"
+            v-model="webPage"
+            placeholder="WEBOVÁ STRÁNKA*"
+            required
+          />
+          <input
+            type="text"
+            class="form_item"
+            name="message"
+            id="message"
+            v-model="message"
+            placeholder="CHCEME VÁM VZKÁZAT"
+            required
+          />
+          <button class="btn" type="submit">PŘIDEJTE SE K ZÁSOBY SEM</button>
         </form>
       </div>
     </div>
@@ -25,24 +60,49 @@
 
 <script>
 export default {
-  name: "Pro__rozvozce"
+  name: "Pro__rozvozce",
+  methods: {
+    sendEmails() {
+      // sending email to the customer
+      Email.send({
+        Host: "smtp.gmail.com",
+        Username: this.myMail,
+        Password: "czechitas2020",
+        To: this.email,
+        From: this.myMail,
+        Subject: "novy Vítejte na Zásoby sem",
+        // Body: `Vítejte na zásoby sem! Váš obchod ${this.shopName} už bude brzy rozvážet s námi, hurá! Co nejdříve se s Vámi spojíme! Tým Zásoby sem `,
+        Body: "<table border=1> <tr><td>blabla</td></tr> </table>"
+      });
+      // sending email to myself about the new request
+      const FormattedMessage = this.message
+        ? `P.S.: Abych nezapomněla, ještě vzkazují: "${this.message}".`
+        : "";
+      Email.send({
+        Host: "smtp.gmail.com",
+        Username: this.myMail,
+        Password: "czechitas2020",
+        To: this.myMail,
+        From: this.myMail,
+        Subject: "Další obchod chce k nám na stránku!",
+        Body: `Hurááá. Otevírají se nám nové obzory! Právě jsme obdrželi novou žádost o zařazení obchodu ${this.shopName} mezi naše zásobovače! Koukni na jejich webové stránky tady: ${this.webPage}. A pak honem napiš na email ${this.email}, kontaktní osoba je ${this.name}. Zásoby sem jedou!!! ${FormattedMessage}`
+      }).then(message =>
+        alert("Už to k nám jede! Těšíme se, jak to spolu rozjedem!")
+      );
+    }
+  }
 };
 </script>
 
 <style scoped>
-
- .name,
- .eshop__address,
- .person,
- .email,
- .phone__number {
-    width: 100%;
-    border: 1px solid #9d9065;
-    color: #9d9065;
-    font-family: Cabin;
-    text-align: center;
-    margin: 1vh;
-    padding: 1vh;
+.form_item {
+  width: 100%;
+  border: 1px solid #9d9065;
+  color: #9d9065;
+  font-family: Cabin;
+  text-align: center;
+  margin: 1vh;
+  padding: 1vh;
 }
 
 ::placeholder {
@@ -50,26 +110,28 @@ export default {
   opacity: 1; /* Firefox */
 }
 
-:-ms-input-placeholder { /* Internet Explorer 10-11 */
- color: #9d9065;
+:-ms-input-placeholder {
+  /* Internet Explorer 10-11 */
+  color: #9d9065;
 }
 
-::-ms-input-placeholder { /* Microsoft Edge */
- color: #9d9065;
+::-ms-input-placeholder {
+  /* Microsoft Edge */
+  color: #9d9065;
 }
 
 input:focus {
-  outline-color:#ffe355;
+  outline-color: #ffe355;
 }
 
 .btn {
-    width: 100%;
-    background-color: #9d9065;
-    color: #fff9e8;
-    font-family: Cabin;
-    text-align: center;
-    margin: 1vh;
-    padding: 1vh;
+  width: 100%;
+  background-color: #9d9065;
+  color: #fff9e8;
+  font-family: Cabin;
+  text-align: center;
+  margin: 1vh;
+  padding: 1vh;
 }
 
 .btn:focus {
@@ -91,7 +153,7 @@ h1 {
   margin: 3em;
   padding: 1em;
   opacity: 0.75;
-  box-shadow: 0 0 10px 8px #9D9065;
+  box-shadow: 0 0 10px 8px #9d9065;
   background-color: #fff9e8;
 }
 
@@ -117,14 +179,10 @@ h1 {
   box-shadow: 0 0 10px 8px #9d9065;
 }
 
-
 @media screen and (min-width: 576px) and (max-width: 991px) {
-
 }
 
 @media screen and (min-width: 992px) {
-
-
   .background__image {
     min-height: 73vh;
     flex-basis: 62vw;
