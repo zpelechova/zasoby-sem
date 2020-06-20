e2a47a348bbdc78bab6a19dff54b53e7e74d24b6
Zuzana Pelechová <zpelechova@gmail.com>
2020-06-20 14:52:51 +0200

    recovery delivery

--- src/components/Delivery.vue
+++ src/components/Delivery.vue
@@ -3,47 +3,37 @@
     <div class="background__image">
       <div class="content">
         <p>NÁKUP NA VAŠI ADRESU VÁM MŮŽE PŘIVÉZT</p>
-        <p>Mezi rozvozovými časy eshopů přepínejte přes logo rozvozce.</p>
+        <!-- <p>Mezi rozvozovými časy eshopů přepínejte přes logo rozvozce.</p> -->
         <div class="retailer__logos">
-          <a href="https://www.kosik.cz/" target="_blank">
-            <img class="kosik__logo" border="0" alt="kosik__logo" src="assets\img\kosik_logo.png" />
-          </a>
-          <a href="https://rohlik.cz" target="_blank">
-            <img
-              class="rohlik__logo"
-              border="0"
-              alt="rohlik__logo"
-              src="..\assets\img\rohlik_logo.png"
-            />
-          </a>
-          <a href="https://nakup.itesco.cz/groceries/" target="_blank">
-            <img
-              class="tesco__logo"
-              border="0"
-              alt="tesco__logo"
-              src="..\assets\img\tesco_logo.png"
-            />
-          </a>
+          <div id="kosikReturn">
+            <div id="kosikDelivers"></div>
+            <div id="kosikSlot"></div>
+            <a href="https://www.kosik.cz/" target="_blank">
+              <img class="kosik__logo" border="0" alt="kosik__logo" src="assets\img\kosik_logo.png" />
+            </a>
+          </div>
+          <div id="rohlikReturn">
+            <div id="rohlikDelivers"></div>
+            <div id="rohlikSlot"></div>
+            <a href="https://rohlik.cz" target="_blank">
+              <img
+                class="rohlik__logo"
+                border="0"
+                alt="rohlik__logo"
+                src="assets\img\rohlik_logo.png"
+              />
+            </a>
+          </div>
+          <div id="tescoReturn">
+            <div id="tescoDelivers"></div>
+            <div id="tescoSlot"></div>
+            <a href="https://nakup.itesco.cz/groceries/" target="_blank">
+              <img class="tesco__logo" border="0" alt="tesco__logo" src="assets\img\tesco_logo.png" />
+            </a>
+          </div>
         </div>
-        <p>????? MÁ ČAS PŘIVÉZT ZÁSOBY:</p>
-
-        <p>INFORMACE K ROZVOZU:</p>
-        <p>Tady se mají načítat informace o ceně dopravy, výši minimálního odběru</p>
-        <a
-          href="https://www.kosik.cz/stranky/caste-dotazy?kampan=paticka"
-          target="_blank"
-          class="link"
-        >JAK NAKUPOVAT NA KOŠÍKU</a>
-        <a
-          href="https://www.rohlik.cz/stranka/caste-dotazy"
-          target="_blank"
-          class="link"
-        >JAK NAKUPOVAT NA ROHLÍKU</a>
-        <a
-          href="https://itesco.cz/sluzby-a-znacky/nakupy/vase-prvni-online-nakupy/"
-          target="_blank"
-          class="link"
-        >JAK NAKUPOVAT NA TESCU</a>
+        <p>JEN ČÁST SORIMENTU K VÁM DOVÁŽÍ:</p>
+        <div id="result">{{ result }}</div>
       </div>
     </div>
   </div>
@@ -51,7 +41,67 @@
 
 <script>
 export default {
-  name: "Delivery"
+  name: "Delivery",
+  props: ["street", "city", "zip"],
+  data() {
+    return {
+      loading: true
+    };
+  },
+  methods: {
+    display() {
+      let counter = 2;
+      let results = [];
+      const notify = message => {
+        results.push(message);
+        if (counter === 0) {
+          this.loading = false;
+          console.log(results);
+          // for (let res = 0; res < results.length; res++) {
+          //   result.textContent += results[res];
+          // }
+          router.push("delivery", results);
+        }
+      };
+      // rohlik
+      const rohlikUrl =
+        "https://api.apify.com/v2/acts/zuzka~rohlik/run-sync?token=WDXyEPPmbeKBX5eHAyiszBHQ7&timeout=600";
+
+      fetch(rohlikUrl, {
+        body: `{"street": "${this.street}", "city": "${this.city}", "zip": "${this.zip}"}`,
+        method: "POST",
+        headers: { "Content-Type": "application/json" }
+      })
+        .then(rohlikResp => rohlikResp.json())
+        .then(rohlikJson => {
+          counter--;
+          notify(rohlikJson.message);
+        });
+
+      // kosik
+      const encodeURIParam = stringParam => {
+        return encodeURI(stringParam.replace(/\s/g, "+"));
+      };
+      const kosikUrl = `https://www.kosik.cz/api/web/transport/windows?street=${encodeURIParam(
+        this.street
+      )}&city=${encodeURIParam(this.city)}&zip=${encodeURIParam(this.zip)}`;
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+      fetch(proxyUrl + kosikUrl)
+        .then(kosikResp => kosikResp.json())
+        .then(kosikJson => {
+          // result.textContent = `Nejdříve vám Košík přiveze nákup ${kosikJson.earliest_timeslot}.`;
+          if (kosikJson.times[0] === "8:00 - 18:00") {
+            warning.textContent = " Ale dovážíme jen část sortimentu...";
+          }
+          counter--;
+          notify(kosikJson.earliest_timeslot);
+        });
+    }
+  },
+  beforeMount() {
+    console.log(this.street);
+    // display();
+  }
 };
 </script>
 
@@ -75,7 +125,6 @@ h1 {
   opacity: 0.75;
   box-shadow: 0 0 10px 8px #9d9065;
   background-color: #fff9e8;
-  border-radius: 5%;
 }
 
 .delivery__container {
@@ -99,7 +148,6 @@ h1 {
   background-size: cover;
   background-position: center center;
   box-shadow: 0 0 10px 8px #9d9065;
-  border-radius: 5%;
 }
 
 .kosik__logo,
@@ -123,18 +171,17 @@ h1 {
 
 @media screen and (min-width: 992px) {
   .background__image {
-    height: 62vh;
+    min-height: 73vh;
     flex-basis: 62vw;
     padding: 8vh 0vw 8vh 0vw;
+    border-radius: 5%;
     margin-bottom: 19vh;
   }
-  .homepage__container {
-    height: 62vh;
-    width: 62vw;
-  }
+
   .content {
     width: 32vw;
     margin: auto;
+    border-radius: 5%;
   }
 }
 </style>
