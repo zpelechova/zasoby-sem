ea72361be8fa90fcf188c6bcdd3afc573fd673fc
Zuzana Pelechová <zpelechova@gmail.com>
2020-06-21 14:24:33 +0200

    css changes

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -11,9 +11,10 @@
               name="street"
               v-model="street"
               placeholder="ULICE A ČÍSLO"
+              required
             />
-            <input type="text" class="form_item" name="city" v-model="city" placeholder="OBEC" />
-            <input type="text" class="form_item" name="zip" v-model="zip" placeholder="PSČ" />
+            <input type="text" class="form_item" name="city" v-model="city" placeholder="OBEC" required/>
+            <input type="text" class="form_item" name="zip" v-model="zip" placeholder="PSČ" required />
           </div>
           <div>
             <button class="btn" type="submit">POSLAT</button>
